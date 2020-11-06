be8085a9e518be416b8350803fb8d63a7d32708c
Yko-koshka <legezaanna7@gmail.com>
2020-11-06 16:39:07 +0200

    add styles

--- js/main.js
+++ js/main.js
@@ -1,54 +1,52 @@
 
-    // modal
+// modal
 
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-    modal = document.querySelector('.overlay'),
-    modalClose = document.querySelector('[data-close]');
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+modal = document.querySelector('.overlay'),
+modalClose = document.querySelector('[data-close]');
 
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.toggle('show');
-            document.body.style.overflow = 'hidden';
-        });
-    })
-
-    function closeModal() {
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
         modal.classList.toggle('show');
-        document.body.style.overflow = '';
-    }
-
-    // modalClose.addEventListener('click', closeModal);
-
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            closeModal();
-        }
-    })
-
-    // burger
-
-    const menu = document.querySelector('.header__inner-nav-lists'),
-    menuItem = document.querySelectorAll('.header__inner-nav-list'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger-active');
-        menu.classList.toggle('header__inner-nav-lists-active');
+        document.body.style.overflow = 'hidden';
     });
+})
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger-active');
-            menu.classList.toggle('header__inner-nav-lists-active');
-        })
+function closeModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+}
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+})
+
+// burger
+
+const menu = document.querySelector('.header-inner__lists'),
+menuItem = document.querySelectorAll('.header-inner__list'),
+hamburger = document.querySelector('.header-hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('header-hamburger__active');
+    menu.classList.toggle('header-inner__lists-active');
+});
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('header-hamburger__active');
+        menu.classList.toggle('header-inner__lists-active');
     })
+})
 
-    // slider
+// slider
 
 
-const slides = document.querySelectorAll(".slider__inner-wrapper"),
-  dotsWrap = document.querySelector(".slider__indicators"),
-  dots = document.querySelectorAll(".slider__indicators-dot");
+const slides = document.querySelectorAll(".slider-wrapper"),
+  dotsWrap = document.querySelector(".slider-indicators"),
+  dots = document.querySelectorAll(".slider-indicators__dot");
 
 let index = 0;
 
@@ -62,17 +60,17 @@ function showSlides(n) {
 
   slides.forEach((item) => (item.style.display = "none"));
 
-  dots.forEach((item) => item.classList.remove("slider__indicators-dot-active"));
+  dots.forEach((item) => item.classList.remove("slider-indicators__dot-active"));
 
   slides[index].style.display = "block";
-  dots[index].classList.add("slider__indicators-dot-active");
+  dots[index].classList.add("slider-indicators__dot-active");
 }
 
 showSlides(index);
 
 dotsWrap.addEventListener("click", function (e) {
   for (let i = 0; i < dots.length; i++) {
-    if (e.target.classList.contains("slider__indicators-dot") && e.target == dots[i]) {
+    if (e.target.classList.contains("slider-indicators__dot") && e.target == dots[i]) {
       index = i;
       showSlides(index);
     }
